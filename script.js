9f01c1ce0fa4420cf52f4733875c392dd6395813
BasedAtlas <tsaiwilliam888@gmail.com>
2023-09-06 16:18:34 -0700

    Added basic variables

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-// Assignment Code
+var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", "."];
+
+var characterLength = 0;
+var characterLengthPrompt = 0;
+var promptChoices = [];
+var passwordLength = 0;
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
